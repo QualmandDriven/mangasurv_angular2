f6ff0afcb9808db714b083caa18a177e3fffa226
QualmandDriven <QualmandDriven@outlook.com>
2017-05-09 21:32:32 +0200

    SVG bundled

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -7,7 +7,8 @@ import { AnimeService } from "../animes/anime.service";
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.css'],
+  providers: [MangaService, AnimeService],
 })
 export class AdminComponent implements OnInit {
 
