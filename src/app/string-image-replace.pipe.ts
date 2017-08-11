155a54d568ad3ca308c9de84010fe7816f7d9da1
QualmandDriven <QualmandDriven@outlook.com>
2017-08-11 17:35:23 +0200

    Changed style and images

--- src/app/string-image-replace.pipe.ts
+++ src/app/string-image-replace.pipe.ts
@@ -7,7 +7,7 @@ export class StringImageReplacePipe implements PipeTransform {
 
   transform(value: string): string {
     if(value != undefined) {
-      return value.replace(/[ :+-]/g, "_").replace(/\(|\)/g, "_").replace(/\'/g, "_");
+      return value.replace(/[ :+-]/g, "_").replace(/\(|\)/g, "_").replace(/\'/g, "_").replace(/!/g, "_");
     }
     return value;
   }
